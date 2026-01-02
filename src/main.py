c6273524851ec390ced1091bb17b8cbc6678d5e0
Celdredge <haydendhill01@gmail.com>
2026-01-01 23:39:48 -0700

    IT WORKS! It handles errors cleanly too.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,11 @@ from google.cloud.storage import Blob
 from json import loads, dumps
 from dataclasses import dataclass, field
 
+#-----------
+# Dataclass and State
+#------------
+
+
 #Dataclass for storing instances of variables. A full class isn't quite needed here. (Maybe in the future.)
 @dataclass
 class StorageState:
@@ -17,8 +22,13 @@ class StorageState:
 
 state = StorageState()
 
-def catch_errors(func):
-    """Wrapper that handles try/except in functions using the specified Tuple schema.
+
+#-----------
+# Try/except Wrapper
+#-----------
+
+def catch_errors_2(func):
+    """Wrapper that handles try/except in functions using the specified Tuple schema. Assumes 2 things in tuple.
 
     Args:
         func (function): name of the function to wrap. Usually automatic with the @ syntax.
@@ -27,8 +37,28 @@ def catch_errors(func):
         try:
             return func(*args,**kwargs)
         except Exception as e:
+            
             return (False, str(e))
     return catch
+
+def catch_errors_3(func):
+    """Wrapper that handles try/except in functions using the specified Tuple schema. Assumes 2 things in tuple.
+
+    Args:
+        func (function): name of the function to wrap. Usually automatic with the @ syntax.
+    """
+    def catch(*args, **kwargs):
+        try:
+            return func(*args,**kwargs)
+        except Exception as e:
+            
+            return (False, str(e),None)
+    return catch
+
+#---------
+# Setup functions
+#----------
+
 
 def setup(bucket_n:Optional[str]) -> Tuple[bool, Optional[str]]:
     """Function to set up storage
@@ -52,7 +82,7 @@ def setup(bucket_n:Optional[str]) -> Tuple[bool, Optional[str]]:
     #Set up our stuff
     try:
         #Get the client
-        state.client = storage.Client()
+        state.client = storage.Client(project="project-1-483104")
 
         #Set the state of our client
         state.bucket_name = bucket_n
@@ -62,6 +92,10 @@ def setup(bucket_n:Optional[str]) -> Tuple[bool, Optional[str]]:
 
         #Attach our blob
         state.blob_r = state.bucket.blob(state.blob_name)
+
+        #Ensure the file exists.
+        if not state.blob_r.exists():
+            state.blob_r.upload_from_string("{}")
 
     except Exception as e:
 
@@ -75,7 +109,12 @@ def setup(bucket_n:Optional[str]) -> Tuple[bool, Optional[str]]:
     
     return (True,None)
 
-@catch_errors
+#----------
+# Main Note functions
+#----------
+
+
+@catch_errors_2
 def add_note(title:str,content:str) -> Tuple[bool,Optional[str]]:
     """Function that adds notes to the cloud storage via JSON.
 
@@ -119,7 +158,89 @@ def add_note(title:str,content:str) -> Tuple[bool,Optional[str]]:
         
     return (True,None)
 
+@catch_errors_3
+def get_note(id:Optional[str] = None) -> Tuple[bool,Optional[str],Optional[dict]]:
+    """Function that gets notes from the cloud and serves them.
+
+    Args:
+        id (Optional[int]): ID of the note. If not provided, will get all notes.
+
+    Returns:
+        Tuple[bool,Optional[str]]: A bool, indicating success or failure of the operation. The str is an error message(if no error, it is None).
+    """
+    #Make sure setup has run
+    if state.client is None or state.blob_r is None:
+        return (False, "Setup hasn't run yet.", None)
     
+    #Get the json data
+    notes = loads(state.blob_r.download_as_text())
+
+    #If no id, return the entire notes
+    if id is None:
+        return (True,None,notes)
+    
+    #Check the id to make sure it's good.
+    ok, err = check_int_positive(int(id))
+    if not ok:
+        return(False,str(err),None)
+    
+    entry = notes.get(id,None)
+
+    #If the entry doesn't exist...
+    if entry is None:
+        return (False,"Id does not exist.",None)
+
+    return (True,None,{str(id): entry})
+
+@catch_errors_2
+def delete_note(id:Optional[str]) -> Tuple[bool,Optional[str]]:
+    """Function to delete a note.
+
+    Args:
+        id (Optional[int]): The id of the note that is to be deleted.
+
+    Returns:
+        Tuple[bool,Optional[str]]: A bool, indicating success or failure of the operation. The str is an error message(if no error, it is None).
+    """
+    #If no id, none of the below matters. So we check it.
+    if id is None:
+        return (False, "No id provided.")
+    
+    #Check to see if id is valid number.
+    ok,err = check_int_positive(int(id))
+    if not ok:
+        return (False,str(err))
+    
+    #Make sure setup has run
+    if state.client is None or state.blob_r is None:
+        return (False, "Setup hasn't run yet.")
+    
+    #Get the json data
+    notes = loads(state.blob_r.download_as_text())
+
+
+    #Make sure that the id exists.
+    note = notes.get(id,None)
+
+    #If we didn't find it...
+    if note is None:
+        return (False,"Id does not exist.")
+    
+    #Add the deleted id to our free ids list.
+    state.old_ids.append(int(id))
+
+    #Delete the entry.
+    del notes[str(id)]
+
+    #Update the Cloud Storage.
+    state.blob_r.upload_from_string(dumps(notes))
+
+    return (True,None)
+
+
+#-----------
+#Helper Functions
+#-----------
 
 def generate_id() -> int:
     """Function to generate or take an id from entries that have been deleted.
@@ -143,6 +264,11 @@ def generate_id() -> int:
         state.id_count += 1
 
         return back
+
+#----------------
+# Verification Functions
+#----------------
+
 
 def check_int_positive(*args:Optional[int]) -> Tuple[bool,Optional[str]]:
     """Function to check that an integer is valid for purposes of specifying id.
